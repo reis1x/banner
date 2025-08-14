dc326077d6a66b285d18c99889e4558ae6fa9d6e
reis1x <araujoreishenrique23@gmail.com>
2025-08-14 10:21:51 -0300

    Add files via upload

---
+++ banner2.ps1
@@ -0,0 +1,17 @@
+```
+function bannernovo {
+clear
+echo ""
+echo ""
+Write-Host -ForegroundColor DarkRed "___________                      __            ____        "
+Write-Host -ForegroundColor Red     "\__    ___/_  _  __ ____ _____  |  | __  _____/_   |__  ___"
+Write-Host -ForegroundColor Yellow    "  |    |  \ \/ \/ // __ \\__  \ |  |/ / /  ___/|   \  \/  /"
+Write-Host -ForegroundColor Green     "  |    |   \     /\  ___/ / __ \|    <  \___ \ |   |>    < "
+Write-Host -ForegroundColor Cyan      "  |____|    \/\_/  \___  >____  /__|_ \/____  >|___/__/\_ \"
+Write-Host -ForegroundColor DarkCyan  "                       \/     \/     \/     \/        "
+Write-Host -ForegroundColor Blue      "    \/"
+echo ""
+Write-Host "‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗" -ForegroundColor DarkGray
+echo ""
+echo "}
+```
